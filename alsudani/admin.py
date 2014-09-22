7d11b29c8d082d5a2b08e24429c09b056ee27f8d
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2014-09-22 02:06:24 -0400

    Add markdown to project description

--- alsudani/admin.py
+++ alsudani/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from django.db import models as django_models
+from pagedown.widgets import AdminPagedownWidget
 from . import models
 
 
 class ProjectAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        django_models.TextField: {'widget': AdminPagedownWidget}
+    }
     prepopulated_fields = {'slug': ('project_name', )}
 
 admin.site.register(models.Project, ProjectAdmin)
--- alsudani/settings/base.py
+++ alsudani/settings/base.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdown_deux',
+    'pagedown',
 )
 
 ALLOWED_HOSTS = [
